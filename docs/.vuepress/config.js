8e44ff5264ff33b1b7d50292aeddd81b638318db
ElanYoung <991658923@qq.com>
2021-04-17 14:38:45 +0800

    chore:修改配置选项

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,10 +2,22 @@ module.exports = {
   title: "StarImmortal",
   evergreen: false,
   host: "localhost",
-  port: 8080,
+  port: 3000,
   base: "/",
   dest: "./dist",
-  head: [["link", { rel: "icon", href: "/assets/img/favicon.ico" }]],
+  head: [
+    ["link", { rel: "icon", href: "/favicon.ico" }],
+    ["link", { rel: "manifest", href: "/manifest.json" }],
+    ["meta", { name: "theme-color", content: "#3683d6" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "black" },
+    ],
+    ["link", { rel: "apple-touch-icon", href: "/favicon.ico" }],
+    ["meta", { name: "msapplication-TileImage", content: "/favicon.ico" }],
+    ["meta", { name: "msapplication-TileColor", content: "#3683d6" }],
+  ],
   locales: {
     "/": {},
   },
@@ -19,6 +31,19 @@ module.exports = {
         align: "bottom",
         color: "#3963bc",
         successText: "复制成功~",
+      },
+    ],
+    [
+      "@vuepress/pwa",
+      {
+        serviceWorker: true,
+        updatePopup: {
+          message: "有新内容啦~",
+          buttonText: "刷新",
+        },
+        generateSWConfig: {
+          importWorkboxFrom: "local",
+        },
       },
     ],
     [
@@ -42,7 +67,7 @@ module.exports = {
             name: "music.163.com",
             playListIDs: ["368560770"],
           },
-        ]
+        ],
       },
     ],
   ],
@@ -129,4 +154,4 @@ module.exports = {
       ],
     },
   },
-}
+};
