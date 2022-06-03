0ec81b4802a7bb03747240861f3781fff1ff113f
ElanYoung <991658923@qq.com>
2022-06-03 14:59:29 +0800

    fix: 修复中文锚点跳转问题

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,3 +1,10 @@
 export default ({ Vue, options, router }) => {
-  
+  router.beforeEach((to, from, next) => {
+    if (typeof _hmt !== 'undefined') {
+      if (to.path) {
+        _hmt.push(['_trackPageview', to.fullPath])
+      }
+    }
+    next()
+  })
 }
